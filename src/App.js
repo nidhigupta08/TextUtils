fd9d9baa9e7ec4df906d8b4f1f55103d47abe95a
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-03-24 23:50:42 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import { useState } from 'react';
 import './App.css';
+//import About from './components/About';
+
+
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
 
 function App() {
   const[mode,setMode]=useState('light'); //whether dark mode is enabled or not.
+  
+  const [alert,setAlert]=useState(null);
+
+  const showAlert=(message, type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>
+    {
+      setAlert(null);
+    },800);
+
+  }
  const toggleMode=()=>{
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor='rgb(65 66 68)';
+      showAlert("Dark mode has been enabled", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled", "success");
     }
   }
 
@@ -20,11 +41,13 @@ function App() {
  <>
 {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
 <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+<Alert alert={alert}/>
 <div className="container my-3">
-  <TextForm heading='Enter your text here and select the desired case conversion' mode={mode} />
- {/* <About/>  */}
-</div>
+  <TextForm  showAlert={showAlert} heading='Enter your text here and select the desired case conversion' mode={mode} />
 
+ {/* <About/>  */}
+ 
+</div>
    </>
   );
  
