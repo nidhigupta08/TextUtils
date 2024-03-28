948aa6370c4b5df810e590cd8cbbe0535d593bef
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-03-28 23:33:16 +0530

    Utilized React Router for navigation links.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import {Link} from 'react-router-dom';
 
 //react function based components.
 export default function Navbar(props) {
@@ -11,21 +11,15 @@ export default function Navbar(props) {
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-
-
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+        <Link className="nav-link" to="/">Home</Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/">{props.aboutText}</a>
+        <Link className="nav-link" to="/about">{props.aboutText}</Link>
       </li>
-
-
     </ul>
- 
-   
 {/* template literal or template string in javascript ..ternary operator  */}
 <div className={`custom-control custom-switch text-${props.mode==='light'?'dark':'light'}` }>
   <input type="checkbox" className="custom-control-input "  onClick={props.toggleMode} id="customSwitch1"/>
