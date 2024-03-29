d8052edc6667ce3cafaa27e2950c7dc05f145f49
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-03-29 23:53:51 +0530

    added the color and background color foe the about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,44 +1,53 @@
 import React, { useState } from 'react' 
 
-export default function About() {
-    const[myStyle,setMyStyle]=useState(
-        {
-            color:'black',
-            backgroundColor:'white'
-           
-       }
-    )
-    const[btntext,setBtnText]=useState(
-        "Enable Dark mode"
-    )
-   const toggleStyle=()=>{
-        if(myStyle.color==='black'){
-            setMyStyle({
-                color:'white',
-                backgroundColor:'black',
-               border:'2px solid #56Bfe4'
-           })
-        setBtnText( "Enable Light Mode")  
-        }
-        else{
-            setMyStyle({
-                color:'black',
-                backgroundColor:'white'
-               
-           })
-           setBtnText("Enable Dark Mode")
-          
-        }
+export default function About(props) {
+
+    // const[myStyle,setMyStyle]=useState(
+    //     {
+    //         color:'black',
+    //         backgroundColor:'white'
+    //    }
+    // )
+    // const[btntext,setBtnText]=useState(
+    //     "Enable Dark mode"
+    // )
+
+    let myStyle={
+        color:props.mode==='dark'?'white':'black',
+        backgroundColor:props.mode==='dark'?'rgb(65 66 68)':'white',
     }
+    let myStyle1={
+        backgroundColor:props.mode==='dark'?'rgb(88,88,88)':'white'
+    }
+//    const toggleStyle=()=>{
+//         if(myStyle.color==='black'){
+//             setMyStyle({
+//                 color:'white',
+//                 backgroundColor:'black',
+//                border:'2px solid #56Bfe4'
+//            })
+//         setBtnText( "Enable Light Mode")  
+//         }
+//         else{
+//             setMyStyle({
+//                 color:'black',
+//                 backgroundColor:'white'
+               
+//            })
+//            setBtnText("Enable Dark Mode")
+          
+//         }
+//     }
     return (
+       
         <div className="container" style={myStyle}>
-            <h1>About Us</h1>
-            <div className="accordion my-3"  id="accordionExample">
-                <div className="card">
+            <h1 >About Us</h1>
+            <div className="accordion my-3"  id="accordionExample" >
+                <div className="card" style={myStyle1}>
                     <div className="card-header" id="headingOne" >
                         <h2 className="mb-0">
                             <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={myStyle}>
-                                Collapsible Group Item #1
+                              Analyze your text 
                             </button>
                         </h2>
                     </div>
@@ -49,7 +58,7 @@ export default function About() {
                         </div>
                     </div>
                 </div>
-                <div className="card">
+                <div className="card" style={myStyle1}>
                     <div className="card-header" id="headingTwo">
                         <h2 className="mb-0">
                             <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={myStyle}>
@@ -63,7 +72,7 @@ export default function About() {
                         </div>
                     </div>
                 </div>
-                <div className="card">
+                <div className="card" style={myStyle1}>
                     <div className="card-header" id="headingThree">
                         <h2 className="mb-0">
                             <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={myStyle}>
@@ -78,9 +87,10 @@ export default function About() {
                     </div>
                 </div>
             </div>
-            <div className="my-3">
+            {/* <div className="my-3">
             <button type="button" className='btn btn-primary' onClick={toggleStyle}>{btntext}</button>
-            </div>
+            </div> */}
         </div>
+      
     )
 }
